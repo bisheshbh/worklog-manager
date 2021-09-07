91fe66e06a82601cb2a344c8c76499722768e14b
bisheshbh <bishesh.bhattarai@introcept.co>
2021-09-07 14:44:37 +0545

    Added database credentials in .env files

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,8 +1,9 @@
 import mysql from 'mysql'
+import  {config} from 'dotenv'
 
 export let connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database: 'worklog'
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
