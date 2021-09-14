753ec33b0234edf26102cf3b1b3a36a1ed9bd5c8
bisheshbh <bishesh.bhattarai@introcept.co>
2021-09-14 21:31:58 +0545

    Array destructing implemented in auth middleware

--- src/middleware/auth/auth.middleware.ts
+++ src/middleware/auth/auth.middleware.ts
@@ -28,8 +28,9 @@ class Auth {
                 let user = usersService.decryptCookie(cookie.sid);
                 if(typeof user != 'undefined'){
                     let result:any = await userModel.findOne(user);
-                    if(result.length != 0){
-                        if(result[0].is_admin){
+                       if(result.length != 0){
+                        let [user] = result
+                        if(user.is_admin){
                             return next();
                         }
                     }
